34b3b96ce265d923ebd1bebf414d49bb47c1fbff
Max Bisschop <max@ffkijken.com>
2021-11-21 16:45:15 +0100

    bug

--- config/secrets.php
+++ config/secrets.php
@@ -1,5 +1,4 @@
 <?php
 
 $URI = 'https://talnet.instructure.com/api/graphql';
-// $API_KEY  = "17601~LZ3pktnGAYnvWP222XvIsqjGNY1bg1LfSH1fOfVvmoCAG9AmKX3mDZIyzPBsmnO1iZw";
-// $API_KEY="17601~IojKABP70Rsx3YIZmGBlzQuM2VbjPYog2q222DnNj5DBxwFvxpkQhPdgSOxQj0yrHgP";
+$API_KEY  = "17601~e0MDKSjPUanJsZklBqgz1gzSWgtGo9Jo8PWizZyvfLqOzuFv1D6Fw9VhX7MigjNn";
