a98449e1c15862b69e9f788ca255c6cca6ecbf99
Max Bisschop <max@ffkijken.com>
2021-11-24 00:52:04 +0100

    queries update

--- controllers/QueryController.php
+++ controllers/QueryController.php
@@ -82,9 +82,23 @@ class QueryController extends Controller
         
     }
 
-    public function actionActief($sort='desc', $export=false) {
+    public function actionActief($sort='desc', $export=false, $klas='') {
 
-        $sql="select student_naam Student, klas Klas, max(laatste_activiteit) 'Laatst actief' from resultaat group by 1,2 order by 3 $sort";
+        // $sql="select student_naam Student, klas Klas, max(laatste_activiteit) 'Laatst actief' from resultaat group by 1,2 order by 3 $sort";
+        if ($klas) {
+            $select="and klas='$klas'";
+        } else {
+            $select='';
+        }
+        $sql="
+            SELECT student_naam, klas, module, laatste_beoordeling
+            from resultaat o
+            where laatste_activiteit =
+            (select max(laatste_activiteit) from resultaat i where i.student_nummer=o.student_nummer)
+            and year(laatste_activiteit) > 2020
+            $select
+            order by 4 desc
+        ";
 
         $data=$this->executeQuery($sql, "Laatste activiteit per student", $export);
 
@@ -94,24 +108,68 @@ class QueryController extends Controller
         ]);
     }
 
-    public function actionActiefModule($sort='desc', $export=false) {
+    public function actionWorkingOn($sort='desc', $export=false, $klas='') {
 
-        $sql="select student_naam Student, klas Klas,  module Module, max(laatste_activiteit) 'Laatst actief' from resultaat group by 1,2,3 order by 4 $sort limit 100";
+        if ($klas) {
+            $select="and klas='$klas'";
+        } else {
+            $select='';
+        }
 
-        $data=$this->executeQuery($sql, "Laatste activiteit per student per module", $export);
+        $sql="
+            SELECT module Module, sum(1) studenten
+            from resultaat o
+            where laatste_activiteit =
+            (select max(laatste_activiteit) from resultaat i where i.student_nummer=o.student_nummer)
+            and year(laatste_activiteit) > 2020
+            $select
+            group by 1
+            order by 2 $sort
+        ";
+
+        $data=$this->executeQuery($sql, "Studenten werken aan", $export);
 
         return $this->render('output', [
             'data' => $data,
             'action' => Yii::$app->controller->action->id,
-            'descr' => '(max 100 regels)',
         ]);
     }
 
-    public function actionVoortgang($sort='desc', $export=false) {
+    public function actionModulesFinished($export=false, $klas='') {
 
-        $sql="select student_nummer stdntnr, student_naam Student, klas Klas, SUM(case when voldaan='V' then 1 else 0 end) Voldaan from resultaat group by 1,2, 3 order by 4 $sort";
+        if ($klas) {
+            $select="where klas='$klas'";
+        } else {
+            $select='';
+        }
 
-        $data=$this->executeQuery($sql, "Laatste activiteit per student", $export);
+        $sql="
+            select module Module, sum(case when voldaan='V' then 1 else 0 end) 'Afgerond door'
+            from resultaat o
+            $select
+            group by module
+            order by id, module_id
+        ";
+
+        $data=$this->executeQuery($sql, "Modules voldaan", $export);
+
+        return $this->render('output', [
+            'data' => $data,
+            'action' => Yii::$app->controller->action->id,
+        ]);
+    }
+
+    public function actionVoortgang($sort='desc', $export=false, $klas='') {
+
+        if ($klas) {
+            $select="where klas='$klas'";
+        } else {
+            $select='';
+        }
+
+        $sql="select student_nummer stdntnr, student_naam Student, klas Klas, SUM(case when voldaan='V' then 1 else 0 end) 'Modules voldaan' from resultaat $select group by 1,2, 3 order by 4 $sort";
+
+        $data=$this->executeQuery($sql, "Voortgang", $export);
         
         return $this->render('output', [
             'data' => $data,
