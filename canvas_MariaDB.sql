1d1f3cf1f205577d1c2e925e5e13a4572203cb3b
Max Bisschop <max@ffkijken.com>
2021-11-28 22:58:26 +0100

    db scheme

--- canvas_MariaDB.sql
+++ canvas_MariaDB.sql
@@ -3,19 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 22, 2021 at 04:30 PM
+-- Generation Time: Nov 28, 2021 at 10:57 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `canvas`
@@ -32,7 +26,8 @@ CREATE TABLE `assignment` (
   `points_possible` int(11) NOT NULL,
   `assignment_group_id` int(11) NOT NULL,
   `course_id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL
+  `name` varchar(100) NOT NULL,
+  `position` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -45,6 +40,20 @@ CREATE TABLE `assignment_group` (
   `id` int(11) NOT NULL,
   `course_id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL,
+  `naam` varchar(18) NOT NULL,
+  `korte_naam` varchar(6) NOT NULL,
+  `pos` int(11) NOT NULL,
+  `update_prio` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -65,6 +74,19 @@ CREATE TABLE `module` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `module_def`
+--
+
+CREATE TABLE `module_def` (
+  `id` int(11) NOT NULL,
+  `naam` varchar(80) NOT NULL,
+  `pos` int(11) NOT NULL,
+  `voldaan_rule` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `resultaat`
 --
 
@@ -72,18 +94,20 @@ CREATE TABLE `resultaat` (
   `id` int(11) NOT NULL,
   `course_id` int(11) NOT NULL,
   `module_id` int(11) NOT NULL,
+  `module_pos` int(11) NOT NULL,
   `module` varchar(100) NOT NULL,
   `student_nummer` varchar(8) NOT NULL,
   `klas` varchar(2) DEFAULT NULL,
   `student_naam` varchar(200) NOT NULL,
   `ingeleverd` int(11) NOT NULL,
   `ingeleverd_eo` int(11) NOT NULL,
-  `punten` int(11) NOT NULL,
+  `punten` decimal(4,1) NOT NULL,
   `punten_max` int(11) NOT NULL,
   `punten_eo` int(11) NOT NULL,
   `voldaan` varchar(1) NOT NULL DEFAULT '-',
   `laatste_activiteit` datetime DEFAULT '1970-01-01 00:00:00',
-  `laatste_beoordeling` datetime DEFAULT '1970-01-01 00:00:00'
+  `laatste_beoordeling` datetime DEFAULT '1970-01-01 00:00:00',
+  `aantal_opdrachten` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -102,7 +126,7 @@ CREATE TABLE `submission` (
   `submitted_at` datetime NOT NULL,
   `graded_at` datetime NOT NULL,
   `excused` tinyint(1) NOT NULL,
-  `entered_score` int(11) NOT NULL,
+  `entered_score` decimal(3,1) NOT NULL,
   `workflow_state` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -128,6 +152,30 @@ CREATE TABLE `user` (
 -- Indexes for table `assignment`
 --
 ALTER TABLE `assignment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `assignment_group`
+--
+ALTER TABLE `assignment_group`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `module`
+--
+ALTER TABLE `module`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `module_def`
+--
+ALTER TABLE `module_def`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -161,7 +209,3 @@ ALTER TABLE `user`
 ALTER TABLE `resultaat`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
