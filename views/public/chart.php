5ad427206ae98a71bbdc87e3ffe268c5ab69a327
Max Bisschop <max@ffkijken.com>
2021-12-13 21:15:53 +0100

    chart

---
+++ views/public/chart.php
@@ -0,0 +1,51 @@
+
+    <?php
+
+    function getIsoWeeksInYear($year) {
+        $date = new DateTime;
+        $date->setISODate($year, 53);
+        return ($date->format("W") === "53" ? 53 : 52);
+    }
+
+    $workLoadperWeek=[];
+
+    foreach($data['row'] as $item) { // read all weeks from query into ass. array.
+        $workLoadperWeek[ $item['Week'] ] = intval($item['+Aantal']);
+    }
+
+    $aantalWeken = 20;                                  // Number of weeks in graph
+    $weekNumber = date("W");                            // This week number
+    $weeksThisYear = getIsoWeeksInYear( date("Y") );    // max. week number of this year
+
+    $start = $weekNumber - $aantalWeken;
+    if ($start<0) { // roll over to last year
+        $start += $weeksThisYear;
+    }
+
+    $chartArray=[['Week','Taken']];
+
+   for($i=0; $i<$aantalWeken; $i++){
+       $week=$start+$i;
+       if ( $week > $weeksThisYear ) { // roll over to next year
+           $week -= $weeksThisYear;
+       }
+       if ( array_key_exists($week,$workLoadperWeek) ) {
+            array_push( $chartArray,[ strval($week), intval($workLoadperWeek[$week]) ] ); // value from query
+       } else {
+            array_push( $chartArray,[ strval($week), 0 ]); // no value means 0
+       }
+   }
+
+   //dd($chartArray);
+
+    $chart=[
+        'visualization' => 'LineChart',
+        'data' => $chartArray,
+        'options' => [ 'title' => 'Weekly Activity '.$data['row'][0]['Student'] ]
+    ];
+
+    use scotthuangzl\googlechart\GoogleChart;
+    
+    echo GoogleChart::widget($chart);
+    ?>
+</div>
