2751ea708e097af85ffe46af8adf279dc4e33b9a
Max Bisschop <max@ffkijken.com>
2021-12-21 22:37:49 +0100

    12 weken overzicht

--- controllers/QueryController.php
+++ controllers/QueryController.php
@@ -186,7 +186,8 @@ class QueryController extends Controller
             select
             sum(case when (datediff(curdate(),submitted_at)<=21) then 1 else 0 end) '-21',
             u.klas Klas,
-            concat(u.name,'|/query/submissions|code|',u.code) '!Student',
+            -- concat(u.name,'|/query/submissions|code|',u.code) '!Student',
+            concat(u.name,'|/public/index|code|',u.code) '!Student',
             sum(case when (datediff(curdate(),submitted_at)=1) then 1 else 0 end) '+-2d',
             sum(case when (datediff(curdate(),submitted_at)=0) then 1 else 0 end) '+-1d',
             0 'Graph',
