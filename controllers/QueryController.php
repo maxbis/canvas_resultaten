28a70d4a981b4551bea162f17f5a0a30008f0dcc
Max Bisschop <max@ffkijken.com>
2022-01-10 18:47:41 +0100

    kopjes beoordelingen per docent

--- controllers/QueryController.php
+++ controllers/QueryController.php
@@ -371,10 +371,10 @@ class QueryController extends Controller
     { 
 
         $sql = "
-            SELECT u.name, sum(case when (datediff(curdate(),s.graded_at)<=7) then 1 else 0 end) '+week',
-            sum(case when (datediff(curdate(),s.graded_at)> 7 && datediff(curdate(),s.graded_at)<=14 ) then 1 else 0 end) '+twee weken',
-            sum(case when (datediff(curdate(),s.graded_at)>14 && datediff(curdate(),s.graded_at)<=21 ) then 1 else 0 end) '+drie weken',
-            sum(case when (datediff(curdate(),s.graded_at)>21 && datediff(curdate(),s.graded_at)<=84 ) then 1 else 0 end) '+12 weken'
+            SELECT u.name, sum(case when (datediff(curdate(),s.graded_at)<=7) then 1 else 0 end) '+laatste 7 dagen',
+            sum(case when (datediff(curdate(),s.graded_at)> 7 && datediff(curdate(),s.graded_at)<=14 ) then 1 else 0 end) '+7-14 dagen',
+            sum(case when (datediff(curdate(),s.graded_at)>14 && datediff(curdate(),s.graded_at)<=21 ) then 1 else 0 end) '+14-21 dagen',
+            sum(case when (datediff(curdate(),s.graded_at)>21 && datediff(curdate(),s.graded_at)<=84 ) then 1 else 0 end) '+3-12 weken'
             FROM submission s
             inner join assignment a on s.assignment_id=a.id
             inner join user u on u.id=s.grader_id
