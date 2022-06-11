4698022a05da250dbee43b4019e6c2cf7c96199b
Max Bisschop <max@ffkijken.com>
2022-06-11 22:56:04 +0200

    down

--- web/index.php
+++ web/index.php
@@ -1,16 +1,16 @@
 <?php
 
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
 $signal_file="/tmp/down";
-$down_file="./down.php";
+$down_file=__DIR__ ."/web/down.php";
 
 if ( file_exists($signal_file) ) {
     readfile($down_file);
     exit;
 }
-
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
