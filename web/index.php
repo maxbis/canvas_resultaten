3e6ef12cbde29f78e7b31ae5b61b27c232414842
Max Bisschop <max@ffkijken.com>
2022-06-11 23:02:01 +0200

    down werkt niet

--- web/index.php
+++ web/index.php
@@ -4,13 +4,13 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-$signal_file="/tmp/down";
-$down_file=__DIR__ ."/down.php";
+// $signal_file="/tmp/down";
+// $down_file=__DIR__ ."/down.php";
 
-if ( file_exists($signal_file) ) {
-    readfile($down_file);
-    exit;
-}
+// if ( file_exists($signal_file) ) {
+//     readfile($down_file);
+//     exit;
+// }
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
