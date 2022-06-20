2f0bd50f23fe22953b826275dbbba5406d31b888
Max Bisschop <max@ffkijken.com>
2022-06-20 23:35:19 +0200

    db-switch-c??

--- config/params.php
+++ config/params.php
@@ -1,7 +1,14 @@
 <?php
 
+$subDomain = explode('.', $_SERVER['SERVER_NAME'])[0];
+if ($subDomain == 'www' || $subDomain == 'localhost' ) {
+    $subDomain = 'c21';
+}
+
+
 return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'subDomain' => $subDomain,
 ];
