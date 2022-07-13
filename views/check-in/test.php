6b9bbab1c14e4ff3836aa5ce2fed20ad2b6e36c7
Max Bisschop <max@ffkijken.com>
2022-07-13 20:24:40 +0200

    test page

--- views/check-in/test.php
+++ views/check-in/test.php
@@ -3,13 +3,21 @@
         background-color: #f6f6ff;
     }
 </style>
+<h1>Test pagina</h1>
+<div class="row">
 
 <?php
-$prevKlas="";
+$rows = round((count($result)+1)/3);
+$i=1;
 
-echo "<table class=\"table table-sm hoverTable\">";
+echo "<div class=\"table-responsive col-md-4\">";
+echo "<table class=\"table table-sm hoverTable\" >";
 foreach ($result as $item) {
-    if ( $item['klas'] != $prevKlas ) echo "<tr><td></td><td></td><td></td></tr>";
+    if ( $i++ % $rows == 0 ) {
+        echo "</table></div>";
+        echo "<div class=\"table-responsive col-md-4\">";
+        echo "<table class=\"table table-sm hoverTable\" >";
+    }
     echo "<tr>";
     echo "<td>";
     echo $this->context->button($item['code'], true);
@@ -17,6 +25,8 @@ foreach ($result as $item) {
     echo "<td>".$item['klas']."</td>";
     echo "<td>".$item['name']."</td>";
     echo "</tr>";
-    $prevKlas=$item['klas'];
 }
-echo "</table>";
+echo "</table></div>";
+?>
+
+</div>
