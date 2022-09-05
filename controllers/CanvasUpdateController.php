757aa83aaf9860525b5247139f293dd22cd637e3
Max Bisschop <max-gHub@ffkijken.com>
2022-09-06 00:08:54 +0200

    bug

--- controllers/CanvasUpdateController.php
+++ controllers/CanvasUpdateController.php
@@ -102,7 +102,7 @@ class CanvasUpdateController extends Controller {
             JOIN module_def m on m.id = g.id
             WHERE s.submitted_at > s.graded_at
             AND m.id=$moduleId
-            ORDER BY a.id s.id limit 20
+            ORDER BY a.id, s.id limit 20
         ";
 
         $sqlResult = Yii::$app->db->createCommand($sql)->queryAll();
