e926d30b034a1ce67191d53bc07fb6cfd8f4c86c
Max Bisschop <max-gHub@ffkijken.com>
2022-09-28 15:23:53 +0200

    test

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -36,7 +36,7 @@ class ReportController extends QueryBaseController
             concat(u.name,'|/public/index|code|',u.code) '!Student',
             o.klas Klas, module Module, laatste_activiteit 'Wanneer', datediff(curdate(), laatste_activiteit) 'Dagen'
             FROM user u
-            inner join resultaat o on u.student_nr=o.student_nummer
+            outer join resultaat o on u.student_nr=o.student_nummer
             where laatste_activiteit =
             (select max(laatste_activiteit) from resultaat i where i.student_nummer=o.student_nummer)
             and year(laatste_activiteit) > 2020
