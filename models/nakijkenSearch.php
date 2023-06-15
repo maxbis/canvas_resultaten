800df068dcccc182cd7b9f9499b2ef59fe992890
Max Bisschop <max-gHub@ffkijken.com>
2023-06-15 09:26:26 +0200

    bug

--- models/nakijkenSearch.php
+++ models/nakijkenSearch.php
@@ -4,7 +4,7 @@ namespace app\models;
 
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use app\models\nakijken;
+use app\models\Nakijken;
 
 /**
  * nakijkenSearch represents the model behind the search form of `app\models\nakijken`.
