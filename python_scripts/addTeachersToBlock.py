98a3c7f16301ab715d9b22885ce4a4c26876c793
Max Bisschop <max@ffkijken.com>
2023-06-21 20:08:48 +0200

    scripts

---
+++ python_scripts/addTeachersToBlock.py
@@ -0,0 +1,47 @@
+from canvasapi import Canvas
+import configparser
+
+config = configparser.ConfigParser()
+if ( not (config.read("../import/canvas.ini") or config.read("canvas.ini"))):
+    print()
+    dd('Error: canvas.ini not found')
+
+# Canvas API URL
+API_URL = config.get('main', 'host')
+# Canvas API key
+API_KEY = config.get('main', 'api_key')
+
+canvas = Canvas(API_URL, API_KEY)
+
+# Define your list of teacher emails here
+teacher_emails = ['t.monincx@rocva.nl', 's.krachumott@rocva.nl', 'a.vogel@rocva.nl', 'a.vanderlinden1@rocva.nl', 'y.kurt@rocva.nl', 'm.bisschop@rocva.nl', 't.vanderstam@rocva.nl']
+teacher_ids=['8904', '73267', '8887', '8844', '67247', '8889']
+# teacher_ids=['8904']
+
+# Get the course c24-dev 1 tm 4: 10755 12463 12621 12622  - 12623 12624
+course_id = 12624
+try:
+    course = canvas.get_course(course_id)
+except exceptions.ResourceDoesNotExist:
+    print(f"No course with the ID {course_id} found.")
+    exit(1)
+
+# Iterate over the list of emails
+for id in teacher_ids:
+    print(f"Trying to add {id}")
+    # Search for users with that email
+    user = canvas.get_user(id)
+
+    # If no users found with that email, skip to the next email
+    if not user:
+        print(f"No user found with the email {email}.")
+        continue
+
+    print(user.name)
+
+    # Enroll the teacher in the course
+    result=course.enroll_user(user.id, 'TeacherEnrollment', enrollment_state='active')
+    print(result)
+
+    print(f"Enrolled {user.name} in {course.name}")
+    print()
---
+++ python_scripts/createBlok.py
@@ -0,0 +1,28 @@
+from canvasapi import Canvas
+import configparser
+
+config = configparser.ConfigParser()
+if ( not (config.read("../import/canvas.ini") or config.read("canvas.ini"))):
+    print()
+    dd('Error: canvas.ini not found')
+
+# Canvas API URL
+API_URL = config.get('main', 'host')
+# Canvas API key
+API_KEY = config.get('main', 'api_key')
+
+canvas = Canvas(API_URL, API_KEY)
+
+account = canvas.get_account(82) 
+
+course = account.create_course(
+    course={
+        'name':'Blok 6 - C23',
+        'course_code':'LCT-SD-OP3-C23-25604OR',
+        'default_view': 'modules',
+        'licence': 'private',
+    }
+)
+
+print(f"Created course with ID: {course.id}")
+
