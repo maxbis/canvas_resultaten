2835eb28ea6a7ad71413cb24ba785447d3b0d179
Max Bisschop <max@ffkijken.com>
2023-09-28 20:27:48 +0200

    nakijken form

--- views/nakijken/update.php
+++ views/nakijken/update.php
@@ -1,3 +1,14 @@
+<style>
+    .black-link {
+        color: #000000;
+        text-decoration: none;
+    }
+
+    .black-link:hover {
+        color: #777777;
+    }
+</style>
+
 <?php
 
 use yii\helpers\Html;
@@ -5,14 +16,16 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var app\models\nakijken $model */
 
-$this->title = $model->cohort.' - '.$model->module_name.' - '.$model->assignment_name;
+$this->title = $model->module_name.', '.$model->assignment_name. ' (' . $model->cohort.')';
 $this->params['breadcrumbs'][] = ['label' => 'Nakijken', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->assignment_id, 'url' => ['view', 'assignment_id' => $model->assignment_id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="nakijken-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <?php $link = "https://talnet.instructure.com/courses/" . $model->course_id . "/assignments/" . $model->assignment_id; ?>
+
+    <h3><a href="<?= $link ?>" class="black-link" target="_blank"><?= Html::encode($this->title) ?></a></h2>
 
     <?= $this->render('_form', [
         'model' => $model,
--- views/nakijken/view.php
+++ views/nakijken/view.php
@@ -38,6 +38,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'points_possible',
             'instructie',
             'label',
+            'config'
         ],
     ]) ?>
 
