5009b9e5f73aded0b83535780ef687a94306f7be
Max Bisschop <max@ffkijken.com>
2023-11-09 20:03:02 +0100

    update

--- web/down.php
+++ web/down.php
@@ -3,21 +3,26 @@
 <head>
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 <title>Maintenance</title>
-<style type="text/css">
-      body { text-align: center; padding: 150px; }
-      h1 { font-size: 40px; }
-      body { font: 20px Helvetica, sans-serif; color: #333; }
-      #article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-      a { color: #dc8100; text-decoration: none; }
-      a:hover { color: #333; text-decoration: none; }
-    </style>
+  <style type="text/css">
+        body { text-align: center; padding: 10px; }
+        h1 { font-size: 40px; }
+        body { font: 20px Helvetica, sans-serif; color: #333; }
+        .article { display: block; text-align: left; max-width: 1250px; margin: 0 auto; }
+        .banner { }
+  </style>
 </head>
 <body>
-<div id="article">
-<h1>We zijn op het moment bezig om de resultaten uit Canvas te importeren...</h1>
-<div>
-<p>Oops, just at this very moment we are busy perfoming an import. We'll be back up soon!</p>
-<p>Try again in a few minutes!</p>
-</div>
-</div>
+
+<div class="article">
+  <div class="banner" >
+    <img src="update.png">
+    <h1>We zijn op het moment bezig om de resultaten uit Canvas te importeren...</h1>
+  </div>
+
+  <div>
+    <p>Oops, just at this very moment we are busy perfoming an import. We'll be back up soon!</p>
+    <p>Try again in a few minutes!</p>
+  </div>
+</div> 
+
 </html>
