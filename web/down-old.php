5bd1ca2ff550a8c214a90e87e00e1c15b2fee4e2
Max Bisschop <max@ffkijken.com>
2023-12-19 19:07:59 +0100

    down

---
+++ web/down-old.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+<title>Maintenance</title>
+  <style type="text/css">
+        body { text-align: center; padding: 10px; }
+        h1 { font-size: 40px; }
+        body { font: 20px Helvetica, sans-serif; color: #333; }
+        .article { display: block; text-align: left; max-width: 1250px; margin: 0 auto; }
+        .banner { }
+  </style>
+</head>
+<body>
+
+<div class="article">
+  <div class="banner" >
+    <img src="update.png">
+    <h1>We zijn op het moment bezig om de resultaten uit Canvas te importeren...</h1>
+  </div>
+
+  <div>
+    <p>Oops, just at this very moment we are busy perfoming an import. We'll be back up soon!</p>
+    <p>Try again in a few minutes!</p>
+  </div>
+</div> 
+
+</html>
