4fb0f882bbe8cf7ae4b82002ef2d22aa7c8f4761
Max Bisschop <max@ffkijken.com>
2023-12-19 19:39:53 +0100

    down refresh

--- web/down.php
+++ web/down.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="200">
     <title>Site Maintenance</title>
     <style>
         body {
